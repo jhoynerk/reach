28f86b901507b1eaf961e0ce5f26a8de2dc10b22
Jhoynerk Caraballo <jhoynerk@softwarecriollo.com>
2016-03-02 10:13:37 -0430

    clean

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,16 +6,7 @@ class DashboardController < ApplicationController
     if @built_with.nil?
       return render :text => "Contact Ivan. There are no more leads here"
     end
-
     @potential_client = PotentialClient.new(built_with_id: @built_with.id)
   end
 
-  # Un opportunity
-  # Selecciona 10 BuiltWiths y los associa con un usuario
-  # Los display en el opportunity Controller
-  # Espera 
-  #
-  #
-  # OpportunityManager 
-  # Busca por 
 end
