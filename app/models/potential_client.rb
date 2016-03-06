d43104a2d10d8ce93bb79d769b6c2eaac58fe769
Jhoynerk Caraballo <jhoynerk@softwarecriollo.com>
2016-03-05 20:40:35 -0430

    validates

--- app/models/potential_client.rb
+++ app/models/potential_client.rb
@@ -1,4 +1,7 @@
 class PotentialClient < ActiveRecord::Base
   belongs_to :built_with
   belongs_to :user
+  validates :last_name, :name,  presence: true, length: { maximum: 50 }
+  validates :title, :email,  presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }  
 end
