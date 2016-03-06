ee6c77497768acde5275317da6340f0f268de67f
Jhoynerk Caraballo <jhoynerk@softwarecriollo.com>
2016-03-05 20:39:01 -0430

    fix login

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,21 @@
+var change_class = function (select) {
+  var $this = $(select);
+  if ($this.val())
+    $this.addClass('used');
+  else
+    $this.removeClass('used');
+}
 $(window, document, undefined).ready(function() {
 
-  $('input').blur(function() {
-    var $this = $(this);
-    if ($this.val())
-      $this.addClass('used');
-    else
-      $this.removeClass('used');
+  $('.group > input').blur(function() {
+    change_class($(this));
+  });
+
+  $('.group > input').change(function(){
+    change_class();
+  });
+
+  $('.group > input').focusout(function(){
+    change_class();
   });
 });
