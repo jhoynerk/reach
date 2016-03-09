ace73356c6590e14c5a0d55bdf6086c9abd03213
Jhoynerk Caraballo <jhoynerk@softwarecriollo.com>
2016-03-09 01:38:41 -0430

    Add helper for display class errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Returns the correct class to show messages with bootstrap
+  def flash_class(level)
+    case level
+    when 'notice'  then 'card-panel blue'
+    when 'success' then 'card-panel green'
+    when 'alert'   then 'card-panel yellow'
+    when 'error'   then 'card-panel red'
+    else 'card-panel blue'
+    end
+  end
 end
