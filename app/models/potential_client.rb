76c389dc12b86b35bf4cb3a6aaa02bbce7c89254
Jhoynerk Caraballo <jhoynerk@softwarecriollo.com>
2016-03-11 12:50:31 -0430

    upgrade validates to potential clients

--- app/models/potential_client.rb
+++ app/models/potential_client.rb
@@ -1,9 +1,9 @@
 class PotentialClient < ActiveRecord::Base
   belongs_to :built_with
   belongs_to :user
-  validates :last_name, :name,  presence: true, length: { maximum: 50 }
-  validates :title, :email,  presence: true
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }  
+  validates :last_name, :name,  presence: true, length: { maximum: 50 }, unless: :useless?
+  validates :title, :email,  presence: true, unless: :useless?
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, unless: :useless?
 
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
