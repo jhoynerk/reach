28ffd464edc4be89548374a088e3ba7b75797d02
Jhoynerk Caraballo <jhoynerk@softwarecriollo.com>
2016-03-11 20:23:29 -0430

    add scope to between date and not_useless

--- app/models/potential_client.rb
+++ app/models/potential_client.rb
@@ -4,13 +4,8 @@ class PotentialClient < ActiveRecord::Base
   validates :last_name, :name,  presence: true, length: { maximum: 50 }, unless: :useless?
   validates :title, :email,  presence: true, unless: :useless?
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, unless: :useless?
-
-  def self.to_csv(options = {})
-    CSV.generate(options) do |csv|
-      csv <<  [:id, :name, :email, :last_name, :title]
-      all.select(:id, :name, :email, :last_name, :title).each do |product|
-        csv << product.attributes.values_at(*["id", "name", "email", "last_name", "title"])
-      end
-    end
-  end
+  scope :between_date, -> (d_start, d_end){
+    where(created_at: d_start..d_end)
+  }
+  scope :not_useless, -> { where(useless: false) }
 end
