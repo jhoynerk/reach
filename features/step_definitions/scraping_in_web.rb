88ebcc51003c512cbb9435b37584202d6827eb84
frembering <fremberling@softwarecriollo.com>
2016-04-20 09:55:58 -0430

    Change name on test auth token

--- features/step_definitions/scraping_in_web.rb
+++ features/step_definitions/scraping_in_web.rb
@@ -1,7 +1,7 @@
 When(/^the user sends a web site "([^"]*)" with token authentication$/) do |arg1|
-  expect(ScrapingWeb.authenticity_token(arg1)).to eq(true)
+  expect(ScrapingWeb.exist_token(arg1)).to eq(true)
 end
 
 When(/^the user sends a web site "([^"]*)" without token authentication$/) do |arg1|
-  expect(ScrapingWeb.authenticity_token(arg1)).to eq(false)
+  expect(ScrapingWeb.exist_token(arg1)).to eq(false)
 end
